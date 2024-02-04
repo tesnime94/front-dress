7fffc8326cb8a224a47500e1c25f4904d53cc254
unknown <sihambenarbia2@gmail.com>
2024-02-04 17:56:06 +0100

    recup info user et affiche

--- src/components/user/user.tsx
+++ src/components/user/user.tsx
@@ -1,34 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios'; // Importer axios pour effectuer des requêtes HTTP
 import './user.css';
 
 const UserPage = () => {
   const navigate = useNavigate();
 
   // État pour stocker les informations de l'utilisateur
-  const [userInfo, setUserInfo] = useState({ username: '', email: '',adresse: '',phoneNumber: '',password: ''});
+  const [userInfo, setUserInfo] = useState({ name: '', email: '', address: '', phoneNumber: '', password: '' });
 
   useEffect(() => {
     // Récupérer l'ID de l'utilisateur à partir du localStorage
-    const userId = localStorage.getItem("userId");
+    const userId = localStorage.getItem("userId")?.toString();
     // Faire une requête à votre backend pour récupérer les informations de l'utilisateur correspondant à cet ID
-    // Ici, je simule simplement une requête avec un délai de 1 seconde
-    setTimeout(() => {
-      // Remplacer cet exemple par votre logique de requête réelle
-      const userData = {
-        username: 'JohnDoe',
-        email: 'johndoe@example.com',
-        adresse: '90 rue des roses 75013 Paris',
-        phoneNumber: '0658753510',
-        password: '*******',
-        
-      };
-      // Mettre à jour l'état avec les informations de l'utilisateur récupérées
-      setUserInfo(userData);
-    }, 1000);
+    axios.get(`http://localhost:8080/user/${userId}`) // Modifier l'URL de l'API backend selon votre configuration
+      .then((res) => {
+        // Mettre à jour l'état avec les informations de l'utilisateur récupérées depuis le backend
+        setUserInfo(res.data);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
-  if (!userInfo.username || !userInfo.email) {
+  if (!userInfo.name || !userInfo.email) {
     // Afficher un message de chargement tant que les données de l'utilisateur ne sont pas disponibles
     return <div>Loading...</div>;
   }
@@ -37,9 +30,9 @@ const UserPage = () => {
     <div className=''>
       {/* Afficher les informations de l'utilisateur */}
       <h2>User Information</h2>
-      <p><strong>Username :</strong>{userInfo.username}</p>
+      <p><strong>Username :</strong>{userInfo.name}</p>
       <p><strong>Email :</strong> {userInfo.email}</p>
-      <p><strong>adresse :</strong>{userInfo.adresse}</p>
+      <p><strong>adresse :</strong>{userInfo.address}</p>
       <p><strong>Phone Number :</strong> {userInfo.phoneNumber}</p>
       <p><strong> Password :</strong> {userInfo.password}</p>
       {/* Bouton pour rediriger vers la page de modification */}
